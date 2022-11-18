f20b67fe7a3fd3e67e820c5aeddc216ee0bf1184
Dan Gutt <dgutt10@gmail.com>
2022-11-18 12:10:00 -0600

    Learning about variable declarations in golang

--- main.go
+++ main.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application!")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to our %v booking application!\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("%s has %d tickets\n", userName, userTickets)
 }
