8f0bfd0f163cc1f4cd5fd46c56e7a54f7071d744
Dan Gutt <dgutt10@gmail.com>
2022-11-30 21:11:38 -0600

    Slices

--- main.go
+++ main.go
@@ -13,8 +13,14 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
+	// Setting array with values
 	// var bookings = [50]string{"Dan", "Sam", "Brian"}
-	var bookings = [50]string{}
+
+	// Setting an empty array
+	// var bookings = [50]string{}
+
+	// Setting a slice. Slices are like arrays in ruby
+	var bookings = []string{}
 
 	var firstName string
 	var lastName string
@@ -33,13 +39,18 @@ func main() {
 	fmt.Print("Enter the number of tickets you want: ")
 	fmt.Scan(&userTickets)
 
-	bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 	remainingTickets = remainingTickets - userTickets
 
-	fmt.Printf("The whole array %v\n", bookings)
+	// fmt.Printf("The whole array %v\n", bookings)
+	// fmt.Printf("First value: %v\n", bookings[0])
+	// fmt.Printf("Array type: %T\n", bookings)
+	// fmt.Printf("Array length: %v\n", len(bookings))
+
+	fmt.Printf("The whole slice %v\n", bookings)
 	fmt.Printf("First value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v ticket you will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
